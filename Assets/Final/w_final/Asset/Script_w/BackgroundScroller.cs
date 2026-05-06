217d5fccd854d88d4e6efb78089f69d794f63404
Thitirat12 <thitirat.phua@bumail.net>
2026-05-06 16:47:48 +0700

    Asset Change
    - add overall asset except for player

---
+++ Assets/Final/w_final/Asset/Script_w/BackgroundScroller.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+
+public class BackgroundScroller : MonoBehaviour
+{
+    public float scrollSpeed = 3f;
+    public bool isGameOver = false;
+
+    private float panelWidth;
+    private Vector3 startPos;
+    private Transform clone;
+
+    void Start()
+    {
+        panelWidth = GetComponent<SpriteRenderer>().bounds.size.x;
+        startPos = transform.position;
+
+        // Clone ตัวเองไปวางต่อทางขวาทันที
+        clone = Instantiate(gameObject, new Vector3(
+            startPos.x + panelWidth,
+            startPos.y,
+            startPos.z
+        ), Quaternion.identity).transform;
+
+        // ไม่ให้ Clone สร้าง Clone ซ้ำ
+        Destroy(clone.GetComponent<BackgroundScroller>());
+    }
+
+    void Update()
+    {
+        if (isGameOver) return;
+
+        float move = scrollSpeed * Time.deltaTime;
+        transform.position += Vector3.left * move;
+        clone.position += Vector3.left * move;
+
+        // ถ้าหลุดซ้ายให้วนกลับไปต่อท้าย Clone
+        if (transform.position.x + panelWidth / 2f < startPos.x - panelWidth)
+        {
+            transform.position = new Vector3(
+                clone.position.x + panelWidth,
+                transform.position.y,
+                transform.position.z
+            );
+        }
+
+        if (clone.position.x + panelWidth / 2f < startPos.x - panelWidth)
+        {
+            clone.position = new Vector3(
+                transform.position.x + panelWidth,
+                clone.position.y,
+                clone.position.z
+            );
+        }
+    }
+
+    public void StopScroll() => isGameOver = true;
+    public void StartScroll() => isGameOver = false;
+}
+    
